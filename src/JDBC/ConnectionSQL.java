2d1c2f8ffea39b90741fc164f2738b35c6e09615
ITOPS <jovenreyes682@gmail.com>
2025-02-13 20:07:54 +0800

    Add config.properties template

--- src/JDBC/ConnectionSQL.java
+++ src/JDBC/ConnectionSQL.java
@@ -1,51 +1,41 @@
 package JDBC;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionSQL {
-	
-	
-	private String url;
-	private String user;
-	private String pass;
-	
-	public ConnectionSQL() {
-		 this.setUrl("jdbc:mysql://localhost:3306/studentmanagement");
-		 this.setUser("root");
-		 this.setPass("databaseven");
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getUser() {
-		return user;
-	}
-	public void setUser(String user) {
-		this.user = user;
-	}
-	public String getPass() {
-		return pass;
-	}
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-	
-	public Connection connection() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			return DriverManager.getConnection(getUrl(), getUser(), getPass());
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    
+    private String url;
+    private String user;
+    private String pass;
 
+    public ConnectionSQL() {
+        loadConfig();
+    }
+
+    private void loadConfig() {
+        Properties properties = new Properties();
+        try (FileInputStream fis = new FileInputStream("src/resources/config.properties")) {
+            properties.load(fis);
+            this.url = properties.getProperty("db.url");
+            this.user = properties.getProperty("db.user");
+            this.pass = properties.getProperty("db.password");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection connection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url, user, pass);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
