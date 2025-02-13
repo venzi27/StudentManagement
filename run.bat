b9bb15e87a37e9189091d7693aa7e339265ca953
venzi27 <jovenreyes682@gmail.com>
2025-02-13 22:46:30 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,3 @@
 @echo off
-<<<<<<< HEAD
-cls
-echo Starting Java Program...
-java -cp ".;lib/mysql-connector-j-9.2.0.jar;src" JDBC.Main
+java -cp ".;lib/mysql-connector-j-9.2.0.jar" JDBC.Main
 pause
-=======
-echo Compiling Java files...
-javac -cp ".;lib/mysql-connector-j-9.2.0.jar" -d bin src\JDBC\Main.java
-
-echo Running Java program...
-java -cp ".;lib/mysql-connector-j-9.2.0.jar;bin" JDBC.Main
->>>>>>> ff87f3d1dd00e20551213f4a16d98eff3a481884
